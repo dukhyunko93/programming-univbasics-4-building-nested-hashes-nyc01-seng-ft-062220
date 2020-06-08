3be3eb578a0f0db754bda15ef903ff9c758e2860
Duke Ko <dukhyunko93@gmail.com>
2020-06-08 19:57:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,11 +1,9 @@
 def base_hash
   monopoly = {
     :railroalds => {
-      
+    
     }
-  }# Remember implicit return! We're returning this new Hash _without_ the
-  # keyword "return." Nice and neat.
-
+  }
 end
 
 def monopoly_with_second_tier
