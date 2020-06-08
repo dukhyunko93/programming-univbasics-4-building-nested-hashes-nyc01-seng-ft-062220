6f6e3c900eb0bb573da55ba6eed4ead6d7dd966f
Duke Ko <dukhyunko93@gmail.com>
2020-06-08 20:09:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,7 +9,8 @@ def monopoly_with_second_tier
   monopoly = {
     :railroads => {
       :pieces => 4,
-      :rent_in_dollars => {}
+      :rent_in_dollars => {},
+      :ames => {}
     }
   }
   # When you start writing the implementation for this method, copy the Hash
